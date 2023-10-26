f91b11d7fa989cd17e53e7f7da1d6e0110f3c895
Moongrail <omgone666@yandex.ru>
2023-10-26 19:45:28 +0300

    feat: start

---
+++ taco-client/src/main/java/com/example/tacoclient/TacoClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.tacoclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoClientApplication.class, args);
+	}
+
+}
---
+++ taco-client/src/test/java/com/example/tacoclient/TacoClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tacoclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
