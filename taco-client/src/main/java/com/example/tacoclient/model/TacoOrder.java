f1ade7f6d74095ddf28844a9d7f973ff24f2f64c
Moongrail <omgone666@yandex.ru>
2023-10-27 18:18:08 +0300

    feat: test

---
+++ taco-client/src/main/java/com/example/tacoclient/model/TacoOrder.java
@@ -0,0 +1,28 @@
+package com.example.tacoclient.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TacoOrder {
+    private String deliveryName;
+    private String deliveryStreet;
+    private String deliveryCity;
+    private String deliveryState;
+    private String deliveryZip;
+    private String ccNumber;
+    private String ccExpiration;
+    private String ccCVV;
+    private List<Taco> tacos = new ArrayList<>();
+    public void addTaco(Taco taco) {
+        this.tacos.add(taco);
+    }
+}
